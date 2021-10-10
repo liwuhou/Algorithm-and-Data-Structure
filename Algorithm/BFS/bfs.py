65e35d3d6ad10bc21eb6a3d69523d8ab9166cef7
阿五 <hugewilliam@foxmail.com>
2021-10-10 21:20:43 +0800

    新增广度优先算法的实现

---
+++ Algorithm/BFS/bfs.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+graph = {}
+
+graph['me'] = ['bob', 'alice', 'claire']
+graph['bob'] = ['anuj', 'peggy']
+graph['alice'] = ['peggy']
+graph['claire'] = ['thom', 'jonny']
+graph['anuj'] = []
+graph['bob'] = []
+graph['thom'] = []
+graph['jonny'] = []
+
+def person_is_seller(name):
+  return name[-1] == 'm'
+
+def search(name):
+    searched = []
+    search_queue = deque()
+    search_queue += graph[name]
+
+    while(search_queue):
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_is_seller(person):
+                print('%s is seller' % person)
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+# test
+res = search('me')
+print(res)
+        
