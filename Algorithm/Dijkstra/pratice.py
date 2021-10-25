dafafff7b7de9d9587960bf17f6a997bee5ff5bb
wuhou.li <wuhou.li@tenclass.com>
2021-10-25 08:54:54 +0800

    feat: 新增迪克斯特拉算法的一个实例

--- Algorithm/Dijkstra/pratice.py
+++ Algorithm/Dijkstra/pratice.py
@@ -18,15 +18,21 @@ graph['c']['fin'] = 3
 graph['d'] = {}
 graph['d']['fin'] = 1
 
+graph['fin'] = {}
+
 infinite = float('inf')
 costs = {}
 costs['a'] = 5
 costs['b'] = 2
+costs['c'] = infinite
+costs['d'] = infinite
 costs['fin'] = infinite 
 
 parents = {}
 parents['a'] = 'start'
 parents['b'] = 'start'
+parents['c'] = None
+parents['d'] = None
 parents['fin'] = None
 
 processed = []
@@ -44,4 +50,25 @@ def find_lowest_node(costs):
 def find_lowest_path():
   node = find_lowest_node(costs)
   while node is not None:
-    new_cost = costs[node]
+    cost = costs[node]
+    neighbor = graph[node]
+    for n in neighbor:
+      new_cost = cost + neighbor[n]
+      if costs[n] > new_cost:
+        costs[n] = new_cost
+        parents[n] = node
+    processed.append(node)
+    node = find_lowest_node(costs)
+
+def print_path(parents):
+  res = 'fin'
+  node = parents['fin']
+  while node is not None:
+    res = node + ' -> ' + res
+    node = parents.get(node, None)
+  return res
+
+find_lowest_path()
+print(parents)
+print(costs)
+print(print_path(parents))
