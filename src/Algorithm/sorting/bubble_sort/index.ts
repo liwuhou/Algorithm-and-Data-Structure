b81bf17f5b70551a4cb420c3d971abf1d6338a33
阿五 <hugewilliam@foxmail.com>
2023-06-03 12:15:17 +0800

    feat: Add BubbleSort

---
+++ src/Algorithm/sorting/bubble_sort/index.ts
@@ -0,0 +1,11 @@
+export const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+
+  return arr
+}
